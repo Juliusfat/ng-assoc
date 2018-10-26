94eb4dd1412e664c88c7862c98686c6ebac94a6b
GauthierJK <gauthierjk@gmail.com>
2018-10-26 09:48:41 +0200

    Revert "Create member-add & member-edit"
    This reverts commit 1204d43bb676f0518c25871fd71c1b2051c1fdc6.

--- src/app/core/member/member.module.ts
+++ src/app/core/member/member.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MemberService } from './member.service';
 import { MembersComponent } from './components/members/members.component';
 import { MemberDetailComponent } from './components/member-detail/member-detail.component';
-import { MemberAddComponent } from './components/member-add/member-add.component';
-import { MemberEditComponent } from './components/member-edit/member-edit.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [MembersComponent, MemberDetailComponent, MemberAddComponent, MemberEditComponent],
+  declarations: [MembersComponent, MemberDetailComponent],
   providers: [MemberService]
 })
 export class MemberModule { }
