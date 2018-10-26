9e07d144c37dae86bfc21b9c7f1511e286f7bb0b
GauthierJK <gauthierjk@gmail.com>
2018-10-26 09:52:50 +0200

    Create member-add & member-edit

--- src/app/core/member/member.module.ts
+++ src/app/core/member/member.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MemberService } from './member.service';
 import { MembersComponent } from './components/members/members.component';
 import { MemberDetailComponent } from './components/member-detail/member-detail.component';
+import { MemberAddComponent } from './components/member-add/member-add.component';
+import { MemberEditComponent } from './components/member-edit/member-edit.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [MembersComponent, MemberDetailComponent],
+  declarations: [MembersComponent, MemberDetailComponent, MemberAddComponent, MemberEditComponent],
   providers: [MemberService]
 })
 export class MemberModule { }
