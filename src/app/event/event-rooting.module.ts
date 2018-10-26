5f809a08c3c27270fcdc0ec084abcaad218414b9
blokche <chords-of-life@hotmail.fr>
2018-10-26 09:29:07 +0200

    Adding Base Routing

--- src/app/event/event-rooting.module.ts
+++ src/app/event/event-rooting.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventsComponent } from './components/events/events.component';
+import { EventAddComponent } from './components/event-add/event-add.component';
+import { EventEditComponent } from './components/event-edit/event-edit.component';
 
-const ROUTES:Routes = []
+const ROUTES:Routes = [
+  { path:'', component: EventsComponent },
+  { path:'add', component: EventAddComponent },
+  { path:':id', component: EventsComponent },
+  { path:':id/edit', component: EventEditComponent }
+]
 
 @NgModule({
   imports: [
