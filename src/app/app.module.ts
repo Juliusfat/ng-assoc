a35b9bb2b40f794a8f48609e1a10b48149520172
Juliusfat <cyrille.paillard@free.fr>
2018-10-26 13:11:00 +0200

    fix error event details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CoreModule } from './core/core.module'
 import { AppComponent } from './app.component';
-import { EventDetailComponent } from './event/components/event-detail/event-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EventDetailComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
