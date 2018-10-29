749f9ff27fccf2df9e2a17092551c77b0581117f
blokche <chords-of-life@hotmail.fr>
2018-10-29 09:28:02 +0100

    Update event: done.

--- src/app/event/event.service.ts
+++ src/app/event/event.service.ts
@@ -1,64 +1,70 @@
-import { Injectable } from '@angular/core';
-import { ApiService} from '../core/api/api.service';
-import { environment } from '../../environments/environment';
-import { Event } from './event.model';
-import { Observable } from 'rxjs';
-
+import { Injectable } from "@angular/core";
+import { ApiService } from "../core/api/api.service";
+import { environment } from "../../environments/environment";
+import { Event } from "./event.model";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class EventService {
-  private apiEvent : string = environment.apiUrl + '/events';
+  private apiEvent: string = environment.apiUrl + "/events";
 
-  constructor( private apiService : ApiService) { }
+  constructor(private apiService: ApiService) {}
 
-/**
- * add a new event 
- * @param { Event} event
- * @returns Observable<Event>  
- */
+  /**
+   * add a new event
+   * @param { Event} event
+   * @returns Observable<Event>
+   */
 
-addEvent(event :Event) : Observable<Event> {
-  return this.apiService.http.post<Event>(this.apiEvent, {
-    "id": event.id,
-    "title" : event.title,
-    "date" : event.date,
-    "duration" : event.duration,
-    "capacity" : event.capacity,
-    "participants" : event.participants,
-    "location" : event.location
-   })
-}
+  addEvent(event: Event): Observable<Event> {
+    return this.apiService.http.post<Event>(this.apiEvent, {
+      id: event.id,
+      title: event.title,
+      date: event.date,
+      duration: event.duration,
+      capacity: event.capacity,
+      participants: event.participants,
+      location: event.location
+    });
+  }
 
-/**
- * retrieves the list of events
- * @return Observable<Event>
- */
+  /**
+   * retrieves the list of events
+   * @return Observable<Event>
+   */
 
-getListEvents() :Observable<Event[]>{
-  return this.apiService.http.get<Event[]>(this.apiEvent);
-}
+  getListEvents(): Observable<Event[]> {
+    return this.apiService.http.get<Event[]>(this.apiEvent);
+  }
 
-/**
- * delete one event with its id
- * @param {string} id
- * @returns Observable<Event>
- */
+  /**
+   * delete one event with its id
+   * @param {string} id
+   * @returns Observable<Event>
+   */
 
-deleteEvent( id : string) :Observable<Event>{
-  return this.apiService.http.delete<Event>(this.apiEvent + '/' + id);
+  deleteEvent(id: string): Observable<Event> {
+    return this.apiService.http.delete<Event>(this.apiEvent + "/" + id);
+  }
 
-} 
+  /**
+   * get one event with its id
+   * @param {string} id
+   * @returns Observable<Event>
+   */
 
-/**
- * get one event with its id
- * @param {string} id
- * @returns Observable<Event> 
- */
+  getEventById(id: string): Observable<Event> {
+    return this.apiService.http.get<Event>(this.apiEvent + "/" + id);
+  }
 
-getEventById( id : string ) : Observable<Event>{
-  return this.apiService.http.get<Event>(this.apiEvent + '/' + id);
-}
-
+  /**
+   * Updating an event
+   * @param id
+   * @param values
+   */
+  updateEvent(id: string, values: { [prop: string]: any }) : Observable<Event> {
+    return this.apiService.http.patch<Event>(`${this.apiEvent}/${id}`, values);
+  }
 }
