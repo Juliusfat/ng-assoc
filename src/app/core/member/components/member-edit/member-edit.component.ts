de2e92420ced2664738d2c6ef346f809866ea18e
GauthierJK <gauthierjk@gmail.com>
2018-10-29 09:58:55 +0100

    Update member components (delete & update functions)

--- src/app/core/member/components/member-edit/member-edit.component.ts
+++ src/app/core/member/components/member-edit/member-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MemberService } from '../../member.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Member, Role } from '../../member.model';
 import { Observable } from 'rxjs';
 
@@ -13,19 +13,37 @@ import { Observable } from 'rxjs';
 export class MemberEditComponent implements OnInit {
   member : Member = new Member();
   private id : string;
-  private isAdmin : boolean;
 
-  constructor(private memberService : MemberService, private route : ActivatedRoute) { }
+  constructor(private memberService : MemberService, private route : ActivatedRoute, private router : Router) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.id = params['id']; // Get ID from URL.
       this.memberService.getMemberById(this.id).subscribe(member => {
         this.member = member;
-        if (this.member.role.indexOf(Role.ADMIN) !== -1) {
-          this.isAdmin = true;
-        }
       });
+    });
+  }
+
+  /**
+   * Update member according to his/her id and redirect to member detail.
+   */
+  updateMember() {
+    this.memberService.updateUserById(this.id, this.member).subscribe(member => {
+      if(member) {
+        this.router.navigate(['/members', this.id]);
+      } else {
+        console.log("ERROR : mise à jour du membre échouée.");
+      }
+    });
+  }
+
+  /**
+   * Delete a member according to his/her id and redirect to members list.
+   */
+  deleteMember() {
+    this.memberService.deleteMember(this.id).then(() => {
+      this.router.navigate(['/members']);
     });
   }
 
